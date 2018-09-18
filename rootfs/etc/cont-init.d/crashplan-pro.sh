bec3e8a06d7845efbd3f00c9731c1f3ee1cab3e7
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-09-18 12:57:37 -0400

    Make sure to set ownership on all files/folders, event hidden ones.

--- rootfs/etc/cont-init.d/crashplan-pro.sh
+++ rootfs/etc/cont-init.d/crashplan-pro.sh
@@ -114,6 +114,6 @@ do
 done
 
 # Take ownership of the config directory content.
-chown -R $USER_ID:$GROUP_ID /config/*
+find /config -mindepth 1 -exec chown $USER_ID:$GROUP_ID {} \;
 
 # vim: set ft=sh :

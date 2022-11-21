abd8199fc2a179e8bda738ca1ddbcd22ac00781a
Jocelyn Le Sage <jocelyn@le-sage.com>
2022-11-21 18:59:26 -0500

    Properly kill CrashPlan when receiving a termination signal.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
+trap "exit" TERM QUIT INT
+trap "kill_cp" EXIT
+
+kill_cp() {
+    RC=$?
+    if [ -n "${CP_PID:-}" ]; then
+        kill "$CP_PID"
+        wait $CP_PID
+        exit $?
+    fi
+    exit $RC
+}
+
 while true
 do
     # Start CrashPlan.
     /usr/local/crashplan/bin/startCrashPlanGUI.sh &
 
     # Wait until it dies.
-    wait $!
+    CP_PID=$!
+    wait $CP_PID
     RC=$?
+    CP_PID=
 
     # Exit now if exit was not requested by user.
     if [ ! -f /tmp/.cp_restart_requested ]; then
